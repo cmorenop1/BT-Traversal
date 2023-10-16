9ec55c595e938c8699818570bec8056009da6e65
Cristopher <cristopher.moreno@colibid.com>
2023-10-15 22:35:32 -0400

    commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/binaryTree
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+func (node *Node) PreOrderTraversal() {
+	if node == nil {
+		return
+	}
+
+	// Pre-order traversal (root, left, right)
+	fmt.Println("node.Value:", node.Value)
+	node.Left.PreOrderTraversal()
+	node.Right.PreOrderTraversal()
+}
+
+func (node *Node) InOrderTraversal() {
+	if node == nil {
+		return
+	}
+
+	// In-order traversal (left, root, right)
+	node.Left.InOrderTraversal()
+	fmt.Println("node.Value:", node.Value)
+	node.Right.InOrderTraversal()
+}
+
+func (node *Node) PostOrderTraversal() {
+	if node == nil {
+		return
+	}
+
+	// Post-order traversal (left, right, root)
+	node.Left.PostOrderTraversal()
+	node.Right.PostOrderTraversal()
+	fmt.Println("node.Value:", node.Value)
+}
+
+func CreateNode(value int) *Node {
+	return &Node{
+		Value: value,
+		Left:  nil,
+		Right: nil,
+	}
+}
+
+func (node *Node) AddChildrenAt(parentValue int, left, right *Node) {
+	if node == nil {
+		return
+	}
+
+	if node.Value == parentValue {
+		node.Left = left
+		node.Right = right
+		return
+	}
+
+	node.Left.AddChildrenAt(parentValue, left, right)
+	node.Right.AddChildrenAt(parentValue, left, right)
+}
+
+func main() {
+	tree := CreateNode(7)
+	tree.AddChildrenAt(7, CreateNode(23), CreateNode(3))
+	tree.AddChildrenAt(23, CreateNode(5), CreateNode(4))
+	tree.AddChildrenAt(3, CreateNode(18), CreateNode(21))
+
+	fmt.Println("Pre-order traversal:")
+	tree.PreOrderTraversal()
+	fmt.Println("In-order traversal:")
+	tree.InOrderTraversal()
+	fmt.Println("Post-order traversal:")
+	tree.PostOrderTraversal()
+}
